d05fc943f3a824f6988346465e24c159cc92bb5b
Samuel Toh <stoh6@gatech.edu>
2020-05-08 16:09:00 -0400

    Initial Commit

---
+++ CashApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CashApp
+//
+//  Created by Samuel Toh on 5/8/20.
+//  Copyright © 2020 Samuel Toh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
